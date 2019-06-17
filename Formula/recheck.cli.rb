d897db9ae44e3b57684703c2af9df2f20c66f32d
Daniel Kraus <daniel.kraus@retest.de>
2019-06-17 17:28:22 +0200

    Revert "Try to make build fail for testing purposes"
    This reverts commit 69d0d70f698bf851dbe24dd4261ebab0c18151ab.

--- Formula/recheck.cli.rb
+++ Formula/recheck.cli.rb
@@ -28,6 +28,6 @@ class RecheckCli < Formula
 
   test do
     out = shell_output("#{bin}/recheck")
-    assert_match "FAIL", out
+    assert_match "recheck [--help] [--version] [COMMAND]", out
   end
 end
