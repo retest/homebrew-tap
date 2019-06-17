fdec15c1b262654a85a237f7a14a66d28a1c0712
Daniel Kraus <daniel.kraus@retest.de>
2019-06-17 16:59:01 +0200

    Fix indentation

--- Formula/recheck.cli.rb
+++ Formula/recheck.cli.rb
@@ -1,35 +1,35 @@
 class RecheckCli < Formula
-    desc "Command-line interface for recheck"
-    homepage "https://retest.de/"
-    url "https://github.com/retest/recheck.cli/releases/download/v1.1.0/recheck.cli-1.1.0-bin.zip"
-    sha256 "7dbad4fb9f8bc1063b687e5a550b2010e40783ee739feec14633ed145edc271c"
-  
-    bottle :unneeded
-  
-    depends_on :java => "1.8+"
-  
-    def install
-      # Remove Windows files.
-      rm_f Dir["bin/*.cmd"]
-      # Install required content.
-      libexec.install "lib/"
-      # Create simplified launch script.
-      (bin/"recheck").write <<~EOS
-        #!/usr/bin/env bash
-        exec java -jar #{libexec}/lib/recheck.cli.jar "$@"
-      EOS
-    end
-  
-    def caveats
-      <<~EOS
-        You can obtain an auto-completion script for Bash and ZSH via the completion command.
-        Simply add the resulting output to your .bash_profile and/or .bashrc, for example:
-          $ echo "source <(recheck completion)" >> ~/.bash_profile
-      EOS
-    end
-  
-    test do
-      out = shell_output("#{bin}/recheck")
-      assert_match "recheck [--help] [--version] [COMMAND]", out
-    end
+  desc "Command-line interface for recheck"
+  homepage "https://retest.de/"
+  url "https://github.com/retest/recheck.cli/releases/download/v1.1.0/recheck.cli-1.1.0-bin.zip"
+  sha256 "7dbad4fb9f8bc1063b687e5a550b2010e40783ee739feec14633ed145edc271c"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    # Remove Windows files.
+    rm_f Dir["bin/*.cmd"]
+    # Install required content.
+    libexec.install "lib/"
+    # Create simplified launch script.
+    (bin/"recheck").write <<~EOS
+      #!/usr/bin/env bash
+      exec java -jar #{libexec}/lib/recheck.cli.jar "$@"
+    EOS
   end
+
+  def caveats
+    <<~EOS
+      You can obtain an auto-completion script for Bash and ZSH via the completion command.
+      Simply add the resulting output to your .bash_profile and/or .bashrc, for example:
+        $ echo "source <(recheck completion)" >> ~/.bash_profile
+    EOS
+  end
+
+  test do
+    out = shell_output("#{bin}/recheck")
+    assert_match "recheck [--help] [--version] [COMMAND]", out
+  end
+end
